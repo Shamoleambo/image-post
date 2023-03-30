e119816703f687f10e12793ac3c8d50402e669a4
Tiago Gomes <tidgomes@gmail.com>
2023-03-30 09:06:53 -0300

    serve css file publicly

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const { engine } = require('express-handlebars')
 
@@ -6,6 +7,8 @@ const app = express()
 app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
 app.set('views', './views')
+
+app.use(express.static(path.join(__dirname, '/public')))
 
 app.get('/', (req, res) => {
   res.render('home')
