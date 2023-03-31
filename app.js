629638c96c9d7b7b5ef7a7f7fe80e85e592c9bd2
Tiago Gomes <tidgomes@gmail.com>
2023-03-31 11:29:36 -0300

    refactor app to use routes and clean up the file layout

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 const { engine } = require('express-handlebars')
 
-const Post = require('./model/Post')
+const routes = require('./routes/routes')
 
 const app = express()
 dotenv.config()
@@ -18,34 +18,7 @@ app.use(express.static(path.join(__dirname, '/public')))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-  res.render('home', { pageTitle: 'Home', title: 'Home Page' })
-})
-
-app.get('/register', (req, res) => {
-  res.render('register', {
-    title: 'Register',
-    pageTitle: 'Register',
-    register: true
-  })
-})
-
-app.get('/login', (req, res) => {
-  res.render('login', { title: 'Login', pageTitle: 'Login', login: true })
-})
-
-app.get('/post', (req, res) => {
-  res.render('create-post', { pageTitle: 'New Post', newPost: true })
-})
-
-app.post('/post', async (req, res) => {
-  const { title, image: imageUrl, description } = req.body
-
-  const newPost = new Post({ title, imageUrl, description })
-
-  await newPost.save()
-  res.status(201).render('home', { pageTitle: 'Home', title: 'Home Page' })
-})
+app.use('/', routes)
 
 app.use('/', (req, res) => {
   res.status(404).render('errorPage', { pageTitle: '404' })
